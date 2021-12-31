f275bbd0755ff488d94e14a6a7f8e263503251ff
Max Holloway <41704623+maxholloway@users.noreply.github.com>
2021-12-31 15:53:43 -0600

    add github url

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ footer.innerHTML = `
 <div class="content has-text-centered">
   <p>
     Like this tool? Show your thanks by sending a currency of your choice to an effective charity, such as <a href="https://www.givewell.org/about/donate/cryptocurrency">GiveWell</a>.<br>
-    Have a problem with this tool? Create an issue on <a href="">GitHub</a>, or message the tool's <a href="https://www.linkedin.com/in/maxwell-holloway/">progenitor</a>.
+    Have a problem with this tool? Create an issue on <a href="https://github.com/maxholloway/DyDxRiskStation/">GitHub</a>, or message the tool's <a href="https://www.linkedin.com/in/maxwell-holloway/">progenitor</a>.
   </p>
 </div>
 `;
